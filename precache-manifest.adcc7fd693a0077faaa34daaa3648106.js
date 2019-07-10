d5e563127e185da9153d0ac7d9b9e75063db5539
rkpeltier <rkpeltier@gmail.com>
2019-07-10 16:10:28 -0700

    Updates

---
+++ precache-manifest.adcc7fd693a0077faaa34daaa3648106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2493e4c5b9afec0c03da4caeff078709",
+    "url": "/tarot-shuffle/index.html"
+  },
+  {
+    "revision": "a96c921544d2ec4ac921",
+    "url": "/tarot-shuffle/static/css/main.ac48fd1d.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/tarot-shuffle/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "a96c921544d2ec4ac921",
+    "url": "/tarot-shuffle/static/js/main.cce1a963.chunk.js"
+  },
+  {
+    "revision": "60b15124f78d47962fd4",
+    "url": "/tarot-shuffle/static/js/runtime~main.8b8d33b5.js"
+  }
+]);
